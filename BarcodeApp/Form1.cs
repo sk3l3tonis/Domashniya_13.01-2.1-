efce73ba2e25217cb0e069ae7d2c3ba1bf3c3605
sk3l3tonis <kostik63737@gmail.com>
2024-02-01 16:32:09 +0300

    Добавьте файлы проекта.

---
+++ BarcodeApp/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using BarcodeLib;
+using ZXing;
+using System.Windows.Forms;
+
+namespace BarcodeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BarcodeGenerator barcodeGenerator = new BarcodeGenerator();
+            Bitmap barcode = barcodeGenerator.GenerateBarcode(textBox1.Text, BarcodeFormat.CODE_128, 350, 150);
+            pictureBox1.Image = barcode;
+        }
+    }
+}
